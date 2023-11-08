11516c774e0566650a317853dd10502b0826a6ed
Marius Rundereim <marius.rundereim@gmail.com>
2023-11-08 12:54:43 +0100

    e2e login test

--- cypress/e2e/cy-testing/logout.cy.js
+++ cypress/e2e/cy-testing/logout.cy.js
@@ -1,39 +1,38 @@
 const email = "pertore55@stud.noroff.no";
 const pw = "pertore-55";
 
-// describe("Login", () => {
-//   beforeEach(() => {
-//     cy.visit("/");
-//     cy.clearLocalStorage();
-//     cy.get("#registerModal button")
-//       .contains("Login")
-//       .should("be.visible")
-//       .click()
-//       .wait(500);
-//     cy.get("#registerModal button")
-//       .contains("Close")
-//       .should("be.visible")
-//       .click()
-//       .wait(500);
-//     cy.get("#loginForm input[type='email']")
-//       .should("exist")
-//       .type(email, { delay: 50 });
-//     cy.get("#loginForm input[type='password']")
-//       .should("exist")
-//       .type(pw, { delay: 50 });
-//     cy.get("#loginModal button")
-//       .contains("Login")
-//       .should("be.visible")
-//       .click()
-//       .wait(2000);
-//   });
+describe("Test with correct Authentication", () => {
+  beforeEach(() => {
+    cy.visit("index.html");
+    cy.clearLocalStorage();
+    cy.get("#registerModal button")
+      .contains("Login")
+      .should("be.visible")
+      .click();
+    cy.wait(500);
+    cy.get("#registerModal button")
+      .contains("Close")
+      .should("be.visible")
+      .click();
+    cy.wait(500);
+    cy.get("#loginForm input[type='email']")
+      .should("exist")
+      .type(email, { delay: 50 });
+    cy.get("#loginForm input[type='password']")
+      .should("exist")
+      .type(pw, { delay: 50 });
+    cy.get("#loginModal button").contains("Login").should("be.visible").click();
+    cy.wait(2000);
+  });
 
-//   it("is Logged in", () => {});
-// });
+  it("Can login", () => {
+    expect(localStorage.getItem("token")).to.not.be.null;
+  });
+});
 
 describe("Logout", () => {
   beforeEach(() => {
-    cy.get("#logOutBtn").contains("Logout");
+    cy.get("header button").contains("Logout");
     //   .click()
     //   .should(() => {
     //     expect(localStorage.getItem("token")).to.be.null;
