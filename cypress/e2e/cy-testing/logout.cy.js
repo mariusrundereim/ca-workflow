1a30d8a61da546df6d9b9c996b1da5ce7b2f9957
Marius Rundereim <marius.rundereim@gmail.com>
2023-11-08 14:17:51 +0100

    e2e test done

--- cypress/e2e/cy-testing/logout.cy.js
+++ cypress/e2e/cy-testing/logout.cy.js
@@ -1,43 +1,39 @@
-// const email = "pertore55@stud.noroff.no";
-// const pw = "pertore-55";
+const email = "pertore55@stud.noroff.no";
+const pw = "pertore-55";
 
-// describe("Test with correct Authentication", () => {
-//   beforeEach(() => {
-//     cy.visit("index.html");
-//     cy.clearLocalStorage();
-//     cy.get("#registerModal button")
-//       .contains("Login")
-//       .should("be.visible")
-//       .click();
-//     cy.wait(500);
-//     cy.get("#registerModal button")
-//       .contains("Close")
-//       .should("be.visible")
-//       .click();
-//     cy.wait(500);
-//     cy.get("#loginForm input[type='email']")
-//       .should("exist")
-//       .type(email, { delay: 50 });
-//     cy.get("#loginForm input[type='password']")
-//       .should("exist")
-//       .type(pw, { delay: 50 });
-//     cy.get("#loginModal button").contains("Login").should("be.visible").click();
-//     cy.wait(2000);
-//   });
+describe("Test with correct Authentication", () => {
+  beforeEach(() => {
+    cy.visit("index.html");
+    cy.clearLocalStorage();
+    cy.get("#registerModal button")
+      .contains("Login")
+      .should("be.visible")
+      .click();
+    cy.wait(500);
+    cy.get("#registerModal button")
+      .contains("Close")
+      .should("be.visible")
+      .click();
+    cy.wait(500);
+    cy.get("#loginForm input[type='email']")
+      .should("exist")
+      .type(email, { delay: 50 });
+    cy.get("#loginForm input[type='password']")
+      .should("exist")
+      .type(pw, { delay: 50 });
+    cy.get("#loginModal button").contains("Login").should("be.visible").click();
+  });
 
-//   it("Can login", () => {
-//     expect(localStorage.getItem("token")).to.not.be.null;
-//   });
-// });
+  it("Can login", () => {
+    cy.get("body").should(() => {
+      expect(localStorage.getItem("token")).to.not.be.null;
+    });
+  });
 
-// describe("Logout", () => {
-//   beforeEach(() => {
-//     cy.get("header button").contains("Logout");
-//     //   .click()
-//     //   .should(() => {
-//     //     expect(localStorage.getItem("token")).to.be.null;
-//     //   });
-//   });
-
-//   it("Logout successful", () => {});
-// });
+  it("Can log out", () => {
+    cy.get("#logOutBtn").contains("Logout").should("be.visible").click();
+    cy.get("body").should(() => {
+      expect(localStorage.getItem("token")).to.be.null;
+    });
+  });
+});
